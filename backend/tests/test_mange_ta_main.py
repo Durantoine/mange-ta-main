eeee5479415a57127ee6993d3ad28c4675b538ea
Antoine Durand <antoine.durand@aldebaran.com>
2025-09-30 12:53:44 +0200

    Added data

--- backend/tests/test_mange_ta_main.py
+++ backend/tests/test_mange_ta_main.py
@@ -1,3 +1,4 @@
+from service.layers.domain.mange_ta_main import DataPacket, PacketTypes
 from service.main import struct_logger
 
 
@@ -6,4 +7,4 @@ def test_root_endpoint(test_client):
     struct_logger.info(f"Testing route: {url}")
     response = test_client.get(url)
     assert response.status_code == 200
-    assert response.text == "My name is mange_ta_main!"
+    assert response.json() == DataPacket(type=PacketTypes.RESPONSE, payload="Hi, my name is mange_ta_main!").to_json()
