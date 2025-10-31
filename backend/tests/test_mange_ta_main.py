f3276db445c68d7a4cd25816f0b2b57f3c7d7c50
Paul <paul@MacBook-Pro-de-Paul.local>
2025-10-31 15:01:22 +0100

    fixed backend issues, hopefully for good

--- backend/tests/test_mange_ta_main.py
+++
@@ -1,9 +0,0 @@
-from service.main import struct_logger
-
-
-def test_root_endpoint(test_client):
-    url = "/mange_ta_main/health"
-    struct_logger.info(f"Testing route: {url}")
-    response = test_client.get(url)
-    assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
